ee5822edde4bdae2ece651f29a258c11653d3d11
gsanto <gsanto11@gmail.com>
2015-02-05 18:13:33 +0100

    prima

---
+++ app/src/main/java/com/example/user/p2/GcmBroadcastReceiver.java
@@ -0,0 +1,23 @@
+package com.example.user.p2;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
+/**
+ * Created by user on 05/02/2015.
+ */
+public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Explicitly specify that GcmIntentService will handle the intent.
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                GcmIntentService.class.getName());
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+}
